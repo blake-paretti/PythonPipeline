f88a53eecc0068120a2bbdf7238c8bff163a2a5b
blake-paretti <77642814+blake-paretti@users.noreply.github.com>
2021-01-26 10:43:21 -0600

    Update Unit_Test.py

--- Unit_Test.py
+++ Unit_Test.py
@@ -9,13 +9,13 @@ class TestSum(unittest.TestCase):
         self.assertEqual(add(10, 90), 100)
         
     def test_substract_operation_returns_correct_value(self):
-        self.assertEqual(subtract(100, 90), 10)
+        self.assertEqual(sub(100, 90), 10)
 
     def test_multiply_operation_returns_incorrect_value(self):
-        self.assertEqual(multiply(10, 10), 100)
+        self.assertEqual(mult(10, 10), 100)
 
     def test_division_operation_returns_incorrect_value(self):
-        self.assertEqual(divide(100, 10), 10)
+        self.assertEqual(div(100, 10), 10)
 
 
 if __name__ == '__main__':
